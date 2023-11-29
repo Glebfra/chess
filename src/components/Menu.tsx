871b856ff558c7b64ce57411ede19710447ea13b
glebfra <gleb.frantsuzov@yandex.ru>
2023-11-29 20:56:17 +0300

    + menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import "src/styles/Menu.css";
 
 function Menu(): ReactElement {
 	return (
@@ -12,7 +11,7 @@ function Menu(): ReactElement {
 					<Nav className="me-auto">
 						<Nav.Link href="#friends">Friends</Nav.Link>
 						<Nav.Link href="#4x4">4x4</Nav.Link>
-						<Nav.Link href="1x1">1x1</Nav.Link>
+						<Nav.Link href="#1x1">1x1</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
